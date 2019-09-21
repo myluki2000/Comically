563d581dc6276e9bb545bb690c95027e659afd05
myluki2000 <lukas636@live.com>
2019-09-21 16:47:41 +0200

    modified card outer link to make boundaries of link more accurate. Fixed link underlining for cards

--- Comically/MainModule.cs
+++ Comically/MainModule.cs
@@ -1,24 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-using Comically.data;
-using Nancy.ViewEngines.Razor;
+﻿using Comically.data;
 
 namespace Comically
 {
-    public class MainModule : Nancy.NancyModule
+    public sealed class MainModule : Nancy.NancyModule
     {
         public MainModule()
         {
             Get("/", p => View["index.cshtml", LibraryManager.GetComics()]);
+
             Get("/info/{id}", p =>
             {
                 Comic c = LibraryManager.GetComicById(uint.Parse(p["id"]));
                 return View["info.cshtml", c];
+            });
+
+            Get("/editmeta/{id}", p =>
+            {
+                Comic c = LibraryManager.GetComicById(uint.Parse(p["id"]));
+                return View["editmeta.cshtml", c];
             });
         }
     }
